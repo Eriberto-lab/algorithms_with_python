811ef0dbc7bbd3b1064cc6792dabcf5c5a7b258a
Eriberto Lima <eribertosflima@gmail.com>
2023-07-16 13:44:48 -0300

    feat: adiciona solução com complexidade O(n) para challenge_study

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    '''iniciando projeto'''
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    couter = 0
+
+    for start_time, end_time in permanence_period:
+        if not (isinstance(start_time, int) and isinstance(end_time, int)):
+            return None
+
+        if start_time <= target_time <= end_time:
+            couter += 1
+
+    return couter
