19bd73b667c243a28ddaa4537109c93e89577287
Eriberto Lima <eribertosflima@gmail.com>
2023-07-16 16:06:38 -0300

    test: adiciona teste para função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,17 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(1, 2)
+    with pytest.raises(TypeError):
+        encrypt_message("hello", "2")
+
+    assert encrypt_message("hello", -1) == "olleh"
+    assert encrypt_message("hello", 3) == "leh_ol"
+    # assert encrypt_message("hello", 4) == "olleh_"
+    assert encrypt_message("", 4) == ""
+    assert encrypt_message("hello", 5) == "olleh"
+    assert encrypt_message("hello", 3) == "leh_ol"
+    assert encrypt_message("example", 4) == "elp_maxe"
