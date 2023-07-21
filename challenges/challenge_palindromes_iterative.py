6908c8d950c14b131a425b65b01d87633170cbb8
Eriberto Lima <eribertosflima@gmail.com>
2023-07-21 15:48:28 -0300

    adiciona solução interativa para desafio de palindromos

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+
+    if word[0] != word[-1]:
+        print("Não é palíndromo")
+        return False
+
+    new_word = word[::-1]
+    if word == new_word:
+        return True
+
+    return False
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+
+    if word[low_index] != word[high_index]:
+        print("Não é palíndromo")
+        return False
+
+    new_word = word[::-1]
+    if word == new_word:
+        return True
+
+    return False
