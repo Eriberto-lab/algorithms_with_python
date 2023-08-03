8dd7a4e25ef04483aacc9331386d67c532aa6020
Eriberto Lima <eribertosflima@gmail.com>
2023-08-03 10:22:14 -0300

    feat: adiciona solução (n log n)para resolver o desafio de duplicados

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,29 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def find_duplicate(nums=None):
+    #  - Retorne False se a função não receber nenhum parâmetro;
+    if nums is None:
+        return False
+
+    #  - Retorne False se a função receber como parâmetro uma string;
+    if isinstance(nums, str):
+        return False
+
+    # - Retorne False se a função receber como parâmetro uma lista sem números repetidos;
+    if len(set(nums)) == len(nums):
+        return False
+
+    #  - Retorne False se a função receber como parâmetro apenas um valor;
+    if isinstance(nums, (int, float)):
+        return False
+
+    # - Retorne False se a função receber como parâmetro um número negativo;
+    if any(num < 0 for num in nums):
+        return False
+
+    # - Retorne o número repetivo se a função receber como parâmetro uma lista com números repetidos;
+    nums_dict = {}
+    for num in nums:
+        if num in nums_dict:
+            return num
+        nums_dict[num] = 1
+
+    return False
