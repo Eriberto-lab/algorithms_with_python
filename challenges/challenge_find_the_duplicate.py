cd3f9478ab37d4c8bc853b53474faad5097e5f79
Eriberto Lima <eribertosflima@gmail.com>
2023-08-03 10:32:57 -0300

    refactot: diminuindo complexidade da função find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,25 +1,13 @@
 def find_duplicate(nums=None):
-    #  - Retorne False se a função não receber nenhum parâmetro;
-    if nums is None:
+    if nums is None or isinstance(nums, str):
         return False
 
-    #  - Retorne False se a função receber como parâmetro uma string;
-    if isinstance(nums, str):
+    if len(set(nums)) == len(nums) or isinstance(nums, (int, float)):
         return False
 
-    # - Retorne False se a função receber como parâmetro uma lista sem números repetidos;
-    if len(set(nums)) == len(nums):
-        return False
-
-    #  - Retorne False se a função receber como parâmetro apenas um valor;
-    if isinstance(nums, (int, float)):
-        return False
-
-    # - Retorne False se a função receber como parâmetro um número negativo;
     if any(num < 0 for num in nums):
         return False
 
-    # - Retorne o número repetivo se a função receber como parâmetro uma lista com números repetidos;
     nums_dict = {}
     for num in nums:
         if num in nums_dict:
